de3e6803b912f148cc7ec73b78756248d1c6ed4c
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2018-04-25 22:31:16 -0400

    add mobile menu toggling; refactor js

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,40 @@
 $(document).ready(function () {
-    var $showsSection = $('.shows-section');
-    var $videosSection = $('.videos-section');
-    var $picsSection = $('.pics-section');
-    var $aboutSection = $('.about-section');
-    var $contactSection = $('.contact-section');
+    // Setup Mobile Menu
+    var $body = $('body');
+    var $hamburgerOpenButton = $('.hamburger-open-button');
+    var $xCloseButton = $('.x-close-button');
 
-    var showsTemplate = $('#shows-template').html();
-    var videosTemplate = $('#videos-template').html();
-    var picsTemplate = $('#pics-template').html();
-    var aboutTemplate = $('#about-template').html();
-    var contactTemplate = $('#contact-template').html();
-
-    Handlebars.registerPartial('shows-template', showsTemplate);
-    Handlebars.registerPartial('videos-template', videosTemplate);
-    Handlebars.registerPartial('pics-template', picsTemplate);
-    Handlebars.registerPartial('about-template', aboutTemplate);
-    Handlebars.registerPartial('contact-template', contactTemplate);
-
-    var showsCompiledTemplate = Handlebars.compile(showsTemplate);
-    var videosCompiledTemplate = Handlebars.compile(videosTemplate);
-    var picsCompiledTemplate = Handlebars.compile(picsTemplate);
-    var aboutCompiledTemplate = Handlebars.compile(aboutTemplate);
-    var contactCompiledTemplate = Handlebars.compile(contactTemplate);
-
-    $.ajax({
-        dataType: 'json',
-        url: './data/shows.json'
-      }).done(function(data) {
-        var compiledHtml = showsCompiledTemplate(data);
-        $showsSection.append(compiledHtml);
+    $hamburgerOpenButton.click(function () {
+        console.log('clicked');
+        $body.toggleClass('mobile-menu-open');
     });
 
-    $.ajax({
-        dataType: 'json',
-        url: './data/videos.json'
-      }).done(function(data) {
-        var compiledHtml = videosCompiledTemplate(data);
-        $videosSection.append(compiledHtml);
+    $xCloseButton.click(function () {
+        console.log('clicked');
+        $body.toggleClass('mobile-menu-open');
     });
 
-    $.ajax({
-        dataType: 'json',
-        url: './data/pics.json'
-      }).done(function(data) {
-        var compiledHtml = picsCompiledTemplate(data);
-        $picsSection.append(compiledHtml);
+    // Setup Homepage Sections
+    var sections = ['shows', 'videos', 'about', 'pics', 'contact'];
+    var sectionObjects = sections.map(function (section) {
+        return {
+            sectionName: section,
+            $sectionElement: $('.' + section + '-section'),
+            sectionTemplate: $('#' + section + '-template').html(),
+        }
     });
 
-    $.ajax({
-        dataType: 'json',
-        url: './data/about.json'
-      }).done(function(data) {
-        var compiledHtml = aboutCompiledTemplate(data);
-        $aboutSection.append(compiledHtml);
-    });
+    sectionObjects.forEach(function (sectionObj) {
+        var sectionName = sectionObj.sectionName;
+        Handlebars.registerPartial(sectionName + '-template', sectionObj.sectionTemplate);
+        sectionObj.compiledTemplate = Handlebars.compile(sectionObj.sectionTemplate);
 
-    $.ajax({
-        dataType: 'json',
-        url: './data/contact.json'
-      }).done(function(data) {
-        var compiledHtml = contactCompiledTemplate(data);
-        $contactSection.append(compiledHtml);
+        $.ajax({
+            dataType: 'json',
+            url: './data/' + sectionName + '.json'
+          }).done(function(data) {
+            var compiledHtml =sectionObj.compiledTemplate(data);
+            sectionObj.$sectionElement.append(compiledHtml);
+        });
     });
 })
